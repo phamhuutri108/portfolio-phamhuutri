c470c1e685dffb7c924b28fd7cad667e15a041dd
phamhuutri108 <145194527+phamhuutri108@users.noreply.github.com>
2026-01-09 19:05:47 +0700

    Add files via upload

---
+++ films-data.js
@@ -0,0 +1,136 @@
+/* --- FILE: films-data.js --- */
+
+const filmsData = {
+
+    // --- THÊM MỚI VÀO ĐẦU DANH SÁCH ---
+    "film-dad-lie": {
+        vi: `
+            <p>
+                <b>Thể loại:</b> Phim ngắn hư cấu / Tâm lý<br>
+                <b>Năm:</b> 2024
+            </p>
+            <p>
+                [Nội dung tóm tắt phim Ba ơi đừng nói dối viết ở đây...]
+            </p>
+        `,
+        en: `
+            <p>
+                <b>Genre:</b> Fiction Short / Drama<br>
+                <b>Year:</b> 2024
+            </p>
+            <p>
+                [English synopsis for Dad, don't lie to me...]
+            </p>
+        `
+    },
+
+    // 1. NGHIỆP (KARMA)
+    "film-karma": {
+        vi: `
+            <p>
+                <b>Thể loại:</b> Phim ngắn<br>
+                <b>Giải thưởng:</b> Cánh diều bạc 2024, Hội điện ảnh Việt Nam.
+            </p>
+            <p>
+                [Nội dung tóm tắt phim Nghiệp viết ở đây... Bạn có thể viết rất dài mà không sợ làm rối file HTML chính.]
+            </p>
+        `,
+        en: `
+            <p>
+                <b>Genre:</b> Short Film<br>
+                <b>Awards:</b> Silver Prize, Golden Kite Awards 2024.
+            </p>
+            <p>
+                [English synopsis goes here...]
+            </p>
+        `
+    },
+
+    // 2. CON MA MUỐN SỐNG
+    "film-ghost": {
+        vi: `
+            <p>
+                <b>Thể loại:</b> Phim tài liệu ngắn<br>
+                <b>Giải thưởng:</b> Giải ba, Liên hoan phim ngắn TP. HCM 2023.
+            </p>
+            <p>
+                [Nội dung tóm tắt phim Con Ma Muốn Sống viết ở đây...]
+            </p>
+        `,
+        en: `
+            <p>
+                <b>Genre:</b> Short Documentary<br>
+                <b>Awards:</b> Third Prize, HCMC Short Film Festival 2023.
+            </p>
+            <p>
+                [English synopsis goes here...]
+            </p>
+        `
+    },
+
+    // 3. GÀ TRỐNG NẤU CƠM
+    "film-cocks-cooking": {
+        vi: `
+            <p>
+                <b>Thể loại:</b> Phim ngắn<br>
+                <b>Năm:</b> [Điền năm]
+            </p>
+            <p>
+                [Nội dung tóm tắt phim Gà Trống Nấu Cơm viết ở đây...]
+            </p>
+        `,
+        en: `
+            <p>
+                <b>Genre:</b> Short Film<br>
+                <b>Year:</b> [Year]
+            </p>
+            <p>
+                [English synopsis for Cock's Cooking...]
+            </p>
+        `
+    },
+
+    // 4. KHI CHÚNG TA ĐƯỢC SỐNG (ALIVE)
+    "film-alive": {
+        vi: `
+            <p>
+                <b>Thể loại:</b> Phim ngắn<br>
+                <b>Giải thưởng:</b> Phim xuất sắc nhất, The Original Project 2020.
+            </p>
+            <p>
+                [Nội dung tóm tắt phim Khi Chúng Ta Được Sống...]
+            </p>
+        `,
+        en: `
+            <p>
+                <b>Genre:</b> Short Film<br>
+                <b>Awards:</b> Best Short Film, The Original Project 2020.
+            </p>
+            <p>
+                [English synopsis goes here...]
+            </p>
+        `
+    },
+
+    // 5. KHI THÀNH PHỐ LỚN
+    "film-city": {
+        vi: `
+            <p>
+                <b>Thể loại:</b> Phim tài liệu ngắn<br>
+                <b>Giải thưởng:</b> Giải nhì, Le Filmmaking Camp 2020.
+            </p>
+            <p>
+                [Nội dung tóm tắt phim Khi Thành Phố Lớn...]
+            </p>
+        `,
+        en: `
+            <p>
+                <b>Genre:</b> Short Documentary<br>
+                <b>Awards:</b> Second Prize, Le Filmmaking Camp 2020.
+            </p>
+            <p>
+                [English synopsis goes here...]
+            </p>
+        `
+    }
+};
