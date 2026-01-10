dc3fd129aeab8be8d47d9d937c0db49c65b896fe
phamhuutri108 <phamhuutri108@gmail.com>
2026-01-11 01:39:34 +0700

    Create others-data.js

---
+++ others-data.js
@@ -0,0 +1,62 @@
+/* --- FILE: others-data.js --- */
+
+const othersData = {
+
+    // 1. MUSIC VIDEOS
+    "other-music-videos": {
+        vi: `
+            <p>
+                <b>Thể loại:</b> Music Video (MV)<br>
+                <b>Năm:</b> 2024 - Hiện tại
+            </p>
+
+            <hr style="border: 0; border-top: 1px solid #eee; margin: 20px 0;">
+
+            <div style="margin-bottom: 50px;">
+                <h3>[Tên Bài Hát] - [Tên Ca Sĩ]</h3>
+                <p><i>Vai trò: [Ví dụ: Đạo diễn / Trợ lý sản xuất...]</i></p>
+                
+                <div style="text-align: left; margin: 20px 0;">
+                    <iframe width="100%" height="315" style="max-width: 560px; border-radius: 4px;" 
+                        src="https://www.youtube.com/embed/[ID_VIDEO_YOUTUBE]" 
+                        title="Music Video" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen>
+                    </iframe>
+                </div>
+                <p>
+                    [Mô tả ngắn gọn về dự án này nếu cần...]
+                </p>
+            </div>
+
+            `,
+        en: `
+            <p>
+                <b>Genre:</b> Music Video (MV)<br>
+                <b>Year:</b> 2024 - Present
+            </p>
+
+            <hr style="border: 0; border-top: 1px solid #eee; margin: 20px 0;">
+
+            <div style="margin-bottom: 50px;">
+                <h3>[Song Title] - [Artist Name]</h3>
+                <p><i>Role: [e.g., Director / PA...]</i></p>
+                
+                <div style="text-align: left; margin: 20px 0;">
+                    <iframe width="100%" height="315" style="max-width: 560px; border-radius: 4px;" 
+                        src="https://www.youtube.com/embed/[ID_VIDEO_YOUTUBE]" 
+                        title="Music Video" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen>
+                    </iframe>
+                </div>
+                <p>
+                    [Short description if needed...]
+                </p>
+            </div>
+        `
+    }
+    
+    // Bạn có thể thêm các mục khác ở đây (ví dụ: "other-photography": { ... })
+
+};
