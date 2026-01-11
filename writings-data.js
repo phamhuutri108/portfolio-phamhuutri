efc588e9b6faa6f5d2fda0eede8361d08941a43d
phamhuutri108 <phamhuutri108@gmail.com>
2026-01-12 00:58:21 +0700

    Create writings-data.js

---
+++ writings-data.js
@@ -0,0 +1,270 @@
+/* --- FILE: writing-data.js --- */
+
+// Dùng Mảng (Array) để dễ quản lý thứ tự.
+// Bài mới nhất bạn cứ thêm vào ĐẦU danh sách (ngay sau dấu [ ).
+
+const writingData = [
+    
+    
+    {
+        id: "writings-co-dieu-gi-do-khac",
+        date: "11/06/25", // Tương lai? Hay là 2024? Tôi cứ để nguyên nhé
+        location: "Sài Gòn",
+        title: {
+            vi: "có điều gì đó khác.",
+            en: "something is different."
+        },
+        content: {
+            vi: `<p>Nội dung của bài viết...</p>`,
+            en: `<p>Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...Content of the article...
+            Content of the article...</p>`
+        }
+    },
+
+
+
+    {
+        id: "writings-ba-toi-la-ke-noi-doi",
+        date: "26/11/24",
+        location: "Sài Gòn",
+        title: {
+            vi: "ba tôi là kẻ nói dối.",
+            en: "my father is a liar."
+        },
+        content: {
+            vi: `<p>Nội dung...</p>`,
+            en: `<p>Content...</p>`
+        }
+    },
+
+
+
+    {
+        id: "writings-108-ga-nang-la-ban",
+        date: "11/11/24",
+        location: "Sài Gòn",
+        title: {
+            vi: "108% Gà và Nàng là bạn.",
+            en: "108% Ga and Nang are friends."
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+    {
+        id: "writings-nha-trang-11-09-24",
+        date: "11/09/24",
+        location: "Nha Trang",
+        title: {
+            vi: "Nha Trang, 11/09/24", // Bài này không có tiêu đề, lấy ngày làm tiêu đề
+            en: "Nha Trang, 11/09/24"
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+    {
+        id: "writings-nhung-manh-giay-trong-khe-da",
+        date: "26/06/24",
+        location: "Sài Gòn",
+        title: {
+            vi: "những mảnh giấy trong khe đá.",
+            en: "scraps of paper in the crevices."
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+    {
+        id: "writings-sg-15-04-24",
+        date: "15/04/24",
+        location: "Sài Gòn",
+        title: {
+            vi: "Sài Gòn, 15/04/24",
+            en: "Saigon, 15/04/24"
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+    {
+        id: "writings-vo-tuong-dai",
+        date: "09/03/24",
+        location: "Sài Gòn",
+        title: {
+            vi: "vở tuồng dài",
+            en: "the long play"
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+    {
+        id: "writings-khi-loi-noi-cua-me-dan-tro-nen-theu-thao",
+        date: "05/02/24",
+        location: "Quy Nhơn",
+        title: {
+            vi: "khi những lời nói của mẹ dần trở nên thều thào.",
+            en: "when mother's words became whispers."
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+
+    {
+        id: "writings-sg-12-06-23",
+        date: "12/06/23",
+        location: "Sài Gòn",
+        title: {
+            vi: "Sài Gòn, 12/06/23",
+            en: "Saigon, 12/06/23"
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+
+    {
+        id: "writings-sg-04-03-23",
+        date: "04/03/23",
+        location: "Sài Gòn",
+        title: {
+            vi: "Sài Gòn, 04/03/23",
+            en: "Saigon, 04/03/23"
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+
+    {
+        id: "writings-mui-thuoc-la-quen-thuoc",
+        date: "29/01/2023",
+        location: "Quy Nhơn",
+        title: {
+            vi: "mùi thuốc lá quen thuộc.",
+            en: "that familiar smell of tobacco."
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+
+
+    {
+        id: "writings-tu-do-that-su",
+        date: "17/07/22",
+        location: "Sài Gòn",
+        title: {
+            vi: "tự do thật sự",
+            en: "true freedom"
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+
+    {
+        id: "writings-triet-ly-cai-tran-nha",
+        date: "02/07/22",
+        location: "Sài Gòn",
+        title: {
+            vi: "triết lý cái trần nhà",
+            en: "philosophy of the ceiling"
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+
+
+    {
+        id: "writings-tinh-yeu-di-qua-tinh-thuong-o-lai",
+        date: "28/06/22",
+        location: "Sài Gòn",
+        title: {
+            vi: "tình yêu đi qua, tình thương ở lại",
+            en: "love passes, affection stays"
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    },
+
+
+
+
+    
+    {
+        id: "writings-vong-ngu",
+        date: "19/06/22",
+        location: "Sài Gòn",
+        title: {
+            vi: "vọng ngữ",
+            en: "false speech"
+        },
+        content: {
+            vi: `<p>...</p>`,
+            en: `<p>...</p>`
+        }
+    }
+];
