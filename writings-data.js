2010471168f2e5448fe483a233096dbadcb112ac
phamhuutri108 <phamhuutri108@gmail.com>
2026-01-13 22:41:42 +0700

    Update writings-data.js

--- writings-data.js
+++ writings-data.js
@@ -858,5 +858,47 @@ She looked at me with aching eyes and simply smacked her lips.</p>
     }
 },
 
+
+
+
+
+
+
+
+
+{
+    id: "writings-nha-trang-11-09-24",
+    date: "11/09/24",
+    location: "Quy Nhơn",
+    title: {
+        vi: "Nha Trang, 11/09/24",
+        en: "Nha Trang, 11/09/24"
+    },
+    content: {
+        vi: `
+<p style="margin-bottom: 15px; ">Gửi em ngàn thương mến,</p>
+            <p style="margin-bottom: 15px; ">Cách đây một thời gian, anh cứ nghĩ mình đã mất em và anh đã đau đớn rất nhiều vì sự kiện "lớn" đó - rằng em đã mãi không còn đây. Nhưng, cho đến tận cùng, anh nhận ra rằng anh chưa từng mất gì cả. Anh ngỡ rằng mình mất em, nhưng thật sự anh lại có được em - một con người không "hẳn" mới nhưng chắc chắn đã tìm thấy hạnh phúc giản đơn ở bên cạnh mình - điều mà như em đã nói không còn hiện diện nơi anh. Anh mất em nhưng thật sự anh lại có được em - một bản thể hạnh phúc hơn ngàn lần trước đây và cả sau này, dù cho anh không hiện diện ở đây - bên em, nhưng sự hạnh phúc em đang có, khiến anh nhận ra đó chính là thứ tình thương mà anh mong em sẽ có và luôn xứng đáng được có.</p>
+            <p style="margin-bottom: 15px; ">Thật nhẹ nhõm khi một buổi chiều nắng gắt ở Nha Trang, anh đã xúc động vì sau ngần ấy những sự được-mất thì mình vẫn luôn yêu em như vậy, thương em như vậy và tình yêu dần lớn lên, tỏa ra khắp ngõ ngách của một thành phố lạ. Nơi cũng có biển, cũng có sự nóng bức và ẩm ướt của mồ hôi, đã nhắc nhớ anh về một thời quá vãng, khi ta dạo bộ cùng nhau ở một thành phố biển tương tự tại quê anh.</p>
+            <p style="margin-bottom: 15px; ">Nơi có những hàng thông trăm tuổi, những bà Xơ khấn trọn đời mình cho Thiên Chúa và những ký ức đẹp về chúng ta. Tình yêu trong anh đã lan tỏa và lớn lên từng ngày như vậy.</p>
+            <p style="margin-bottom: 15px; ">Anh hạnh phúc vì anh đã luôn sống cho tình yêu và được là tình yêu - giản đơn, không còn cưỡng cầu và tràn đầy tình thương mến, cho nhau.</p>
+`,
+        en: `
+<p style="margin-bottom: 15px; ">Gửi em ngàn thương mến,</p>
+            <p style="margin-bottom: 15px; ">Cách đây một thời gian, anh cứ nghĩ mình đã mất em và anh đã đau đớn rất nhiều vì sự kiện "lớn" đó - rằng em đã mãi không còn đây. Nhưng, cho đến tận cùng, anh nhận ra rằng anh chưa từng mất gì cả. Anh ngỡ rằng mình mất em, nhưng thật sự anh lại có được em - một con người không "hẳn" mới nhưng chắc chắn đã tìm thấy hạnh phúc giản đơn ở bên cạnh mình - điều mà như em đã nói không còn hiện diện nơi anh. Anh mất em nhưng thật sự anh lại có được em - một bản thể hạnh phúc hơn ngàn lần trước đây và cả sau này, dù cho anh không hiện diện ở đây - bên em, nhưng sự hạnh phúc em đang có, khiến anh nhận ra đó chính là thứ tình thương mà anh mong em sẽ có và luôn xứng đáng được có.</p>
+            <p style="margin-bottom: 15px; ">Thật nhẹ nhõm khi một buổi chiều nắng gắt ở Nha Trang, anh đã xúc động vì sau ngần ấy những sự được-mất thì mình vẫn luôn yêu em như vậy, thương em như vậy và tình yêu dần lớn lên, tỏa ra khắp ngõ ngách của một thành phố lạ. Nơi cũng có biển, cũng có sự nóng bức và ẩm ướt của mồ hôi, đã nhắc nhớ anh về một thời quá vãng, khi ta dạo bộ cùng nhau ở một thành phố biển tương tự tại quê anh.</p>
+            <p style="margin-bottom: 15px; ">Nơi có những hàng thông trăm tuổi, những bà Xơ khấn trọn đời mình cho Thiên Chúa và những ký ức đẹp về chúng ta. Tình yêu trong anh đã lan tỏa và lớn lên từng ngày như vậy.</p>
+            <p style="margin-bottom: 15px; ">Anh hạnh phúc vì anh đã luôn sống cho tình yêu và được là tình yêu - giản đơn, không còn cưỡng cầu và tràn đầy tình thương mến, cho nhau.</p>
+`
+    }
+},
+
+
+
+
+
+
+
+
+
     
 ];
